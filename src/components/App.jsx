3c4e94a10bf172abd38c7035a777497f35f0d5d7
Alex Spirgel <alexs@easternstandard.com>
2021-07-19 12:08:25 -0400

    Styling update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,9 @@ import { DateTime } from 'luxon';
 import PageBody from "./PageBody";
 
 const App = () => {
-  const now = DateTime.local().toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS);
-
   return (
     <div className="app">
-      <header className="persistent-header">persistent header {now}</header>
       <PageBody />
-      <footer className="persistent-footer">persistent footer {now}</footer>
     </div>
   )
 }
--- src/components/node-news/NewsDetails.jsx
+++ src/components/node-news/NewsDetails.jsx
@@ -16,12 +16,32 @@ export const NewsDetails = ({ node }) => {
       });
     }
   }, [node]);
-
+  
   return (
     <div className="node--news node--news--details">
       <Link to='/'>View All News</Link>
-      <h1 className="title">{node.attributes.title}</h1>
-      <PublishDate node={node} />
+      <div class="header component js-viewport-transition entered-viewport in-viewport">
+        <div class="header__color-block">
+          <div class="header__left-container">
+            <div class="header__content-container">
+              <div class="header__text-container">
+                <div class="header__subtitle">Subtitle</div>
+                <h1 class="header__title">{node.attributes.title}</h1>
+                <div class="header__description">A brief description will go here</div>
+              </div>
+            </div>
+          </div>
+          <div class="header__right-container">
+            <div class="header__image-color-block"></div>
+            <div class="header__image-area">
+              <img class="header__image" src={'http://placehold.it/800x600'} alt="Person on ladder working in the forest trees." />
+            </div>
+          </div>
+        </div>
+        <div class="header__caption-container">
+          <div class="header__caption">This is an optional header caption.</div>
+        </div>
+      </div>
       <Paragraphs data={fieldComponents} />
     </div>
   );
