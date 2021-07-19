9bf16a7de4c45ce016ee0e21c9063a689ccac75f
Alex Spirgel <alexs@easternstandard.com>
2021-07-19 12:21:07 -0400

    Added persistent header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,14 @@ import { DateTime } from 'luxon';
 
 import PageBody from "./PageBody";
 
+import logo from './temple-logo-t-box.svg';
+
 const App = () => {
   return (
     <div className="app">
+    	<div class="persistent-header">
+    		<img class="persistent-header__logo" src={logo} />
+    	</div>
       <PageBody />
     </div>
   )
